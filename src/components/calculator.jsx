30f68b66e4be63f859c82214c6886e6597fe0679
João Henrique <jjb9362@gmail.com>
2025-12-01 15:24:19 -0300

    Initial commit

---
+++ src/components/calculator.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+export function Calculator(){
+    const [input, setInput] = useState("");
+
+    const handleClick = (value) => {
+        setInput(input+value);
+    };
+
+    return (
+        <div className="grid shadow-md w-[360px] text-2xl font-bold">
+            <div className="bg-blue-600 text-white min-h-[180px] grow flex flex-col justify-end items-end p-8 gap-4">
+                <span className="flex w-fit justify-self-end text-xl">{input}</span>
+                <div className="flex justify-between w-full items-center text-5xl">
+                    <span>=</span>
+                </div>
+            </div>
+            <div className="bg-blue-300 grow h-[480px] grid grid-cols-4">
+                <button className="justify-center items-center hover:opacity-75 transition ease-in-out duration-300 hover:text-amber-50 bg-blue-500 text-black" onClick={() => handleClick("+")}>
+                    +
+                </button>
+                <button className="justify-center items-center hover:opacity-75 transition ease-in-out duration-300 hover:text-amber-100" onClick={() => handleClick("7")}>
+                    7
+                </button>
+            </div>
+        </div>
+    );
+}
